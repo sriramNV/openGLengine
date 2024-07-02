4ea7c672d6b09733d054a0823878049e20e3e208
Sriram N V <sriram.vilvanathan@gmail.com>
2024-07-02 18:36:01 +0530

    Basic window with Win32

---
+++ OGL3D/source/OGL3D/Game/OGame.cpp
@@ -0,0 +1,31 @@
+#include <OGL3D/Game/OGame.h>
+#include <OGL3D/Window/OWindow.h>
+#include <Windows.h>
+
+OGame::OGame()
+{
+	m_display = std::unique_ptr<OWindow>(new OWindow());
+}
+
+OGame::~OGame()
+{
+}
+
+void OGame::run()
+{
+	MSG msg;
+	while (m_isRunning && !m_display->isClosed()) {
+
+		if (PeekMessage(&msg, NULL, NULL, NULL, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessageW(&msg);
+
+		}
+		Sleep(1);
+	}
+}
+
+void OGame::quit()
+{
+	m_isRunning = false;
+}
